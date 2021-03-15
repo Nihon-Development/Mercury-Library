35a016a8342d4782c8e49458f40751b9b75d4dbf
ImmuneLion318 <54381449+ImmuneLion318@users.noreply.github.com>
2021-03-15 02:06:53 -0400

    Add files via upload

---
+++ Mercury Library/Mercury Library/Functions.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MercuryLibrary
+{
+    public class Functions
+    {
+        public bool IsAttached(string LuaPipe)
+        {
+            return NamedPipes.NamedPipeExist(LuaPipe);
+        }
+
+        public void Inject(string LuaPipe, string DllName)
+        {
+            if (NamedPipes.NamedPipeExist(LuaPipe))
+            {
+                MessageBox.Show("Already Attached", "Error: Already Attached", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return;
+            }
+            else if (!NamedPipes.NamedPipeExist(LuaPipe))
+            {
+                switch (Injector.DllInjector.GetInstance.Inject("RobloxPlayerBeta", AppDomain.CurrentDomain.BaseDirectory + DllName))
+                {
+                    case Injector.DllInjectionResult.DllNotFound:
+                        MessageBox.Show("Dll Missing Or Wasn't Found", "Error: Dll Missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    case Injector.DllInjectionResult.GameProcessNotFound:
+                        MessageBox.Show("Couldn't Find Roblox Game Client", "Error: Roblox Not Found Or Not Opened", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    case Injector.DllInjectionResult.InjectionFailed:
+                        MessageBox.Show("Injection Failed", "Error: Injection Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                }
+            }
+        }
+
+        public void ExecuteScript(string LuaPipe, string DllName, string Script)
+        {
+            if (NamedPipes.NamedPipeExist(LuaPipe))
+            {
+                using (NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", LuaPipe, PipeDirection.Out))
+                {
+                    namedPipeClientStream.Connect();
+                    using (StreamWriter streamWriter = new StreamWriter(namedPipeClientStream, Encoding.Default, 999999))
+                    {
+                        streamWriter.Write(Script);
+                        streamWriter.Dispose();
+                    }
+                    namedPipeClientStream.Dispose();
+                }
+                return;
+            }
+            if (File.Exists(DllName))
+            {
+                MessageBox.Show("Not Attached", "Error: Not Attached", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("Dll Not Found", "Error: Dll Missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    internal class NamedPipes
+    {
+        public static bool NamedPipeExist(string PipeName)
+        {
+            bool result;
+            try
+            {
+                if (!WaitNamedPipe(Path.GetFullPath(string.Format("\\\\.\\pipe\\{0}", PipeName)), 0))
+                {
+                    int lastWin32Error = Marshal.GetLastWin32Error();
+                    if (lastWin32Error == 0)
+                    {
+                        result = false;
+                        return result;
+                    }
+                    if (lastWin32Error == 2)
+                    {
+                        result = false;
+                        return result;
+                    }
+                }
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool WaitNamedPipe(string name, int timeout);
+    }
+
+    internal class Injector
+    {
+        public enum DllInjectionResult
+        {
+            DllNotFound,
+            GameProcessNotFound,
+            InjectionFailed,
+            Success
+        }
+
+        public sealed class DllInjector
+        {
+            static readonly IntPtr INTPTR_ZERO = (IntPtr)0;
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern int CloseHandle(IntPtr hObject);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern IntPtr GetModuleHandle(string lpModuleName);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, uint flAllocationType, uint flProtect);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddress,
+                IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+            static DllInjector _instance;
+
+            public static DllInjector GetInstance
+            {
+                get
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DllInjector();
+                    }
+                    return _instance;
+                }
+            }
+
+            DllInjector() { }
+
+            public DllInjectionResult Inject(string sProcName, string sDllPath)
+            {
+                if (!File.Exists(sDllPath))
+                {
+                    return DllInjectionResult.DllNotFound;
+                }
+
+                uint _procId = 0;
+
+                Process[] _procs = Process.GetProcesses();
+                for (int i = 0; i < _procs.Length; i++)
+                {
+                    if (_procs[i].ProcessName == sProcName)
+                    {
+                        _procId = (uint)_procs[i].Id;
+                        break;
+                    }
+                }
+
+                if (_procId == 0)
+                {
+                    return DllInjectionResult.GameProcessNotFound;
+                }
+
+                if (!bInject(_procId, sDllPath))
+                {
+                    return DllInjectionResult.InjectionFailed;
+                }
+
+                return DllInjectionResult.Success;
+            }
+
+            bool bInject(uint pToBeInjected, string sDllPath)
+            {
+                IntPtr hndProc = OpenProcess((0x2 | 0x8 | 0x10 | 0x20 | 0x400), 1, pToBeInjected);
+
+                if (hndProc == INTPTR_ZERO)
+                {
+                    return false;
+                }
+
+                IntPtr lpLLAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+                if (lpLLAddress == INTPTR_ZERO)
+                {
+                    return false;
+                }
+
+                IntPtr lpAddress = VirtualAllocEx(hndProc, (IntPtr)null, (IntPtr)sDllPath.Length, (0x1000 | 0x2000), 0X40);
+
+                if (lpAddress == INTPTR_ZERO)
+                {
+                    return false;
+                }
+
+                byte[] bytes = Encoding.ASCII.GetBytes(sDllPath);
+
+                if (WriteProcessMemory(hndProc, lpAddress, bytes, (uint)bytes.Length, 0) == 0)
+                {
+                    return false;
+                }
+
+                if (CreateRemoteThread(hndProc, (IntPtr)null, INTPTR_ZERO, lpLLAddress, lpAddress, 0, (IntPtr)null) == INTPTR_ZERO)
+                {
+                    return false;
+                }
+
+                CloseHandle(hndProc);
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Mercury Library/Mercury Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mercury Library")]
+[assembly: AssemblyDescription("Mercury Library For Using Different Dll's Via Pipe Client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nihon Development Team")]
+[assembly: AssemblyProduct("Mercury Library")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("Nihon Development Team")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("598c9d7f-dca8-4704-9981-accfa9eb5218")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
